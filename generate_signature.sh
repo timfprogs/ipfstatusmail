07b4b86422c387ac550192639a5e6dbd22b45f88
timf <40144403+timfprogs@users.noreply.github.com>
2018-09-23 17:17:47 +0100

    Correct localion of keys

--- generate_signature.sh
+++ generate_signature.sh
@@ -4,11 +4,11 @@ source /var/ipfire/dma/mail.conf
 
 # Find the old key if there is one so we can delete it later
 
-OLDKEY=`gpg --homedir /var/ipfire/statusmail/certificates --with-colons --fingerprint --list-keys IPFire | sed -ne '/^fpr/{s/fpr//;s/://g;p}'`
+OLDKEY=`gpg --homedir /var/ipfire/statusmail/keys --with-colons --fingerprint --list-keys IPFire | sed -ne '/^fpr/{s/fpr//;s/://g;p}'`
 
 echo Generate new keys
 
-/usr/bin/gpg --homedir /var/ipfire/statusmail/certificates --batch --gen-key <<EOF
+/usr/bin/gpg --homedir /var/ipfire/statusmail/keys --batch --gen-key <<EOF
 Key-Type: rsa
 Key-Length: 4096
 Key-Usage: sign
@@ -22,6 +22,6 @@ EOF
 
 if [[ $OLDKEY ]]; then
   echo Delete old keys
-  gpg --homedir /var/ipfire/statusmail/certificates --batch --yes --delete-secret-keys $OLDKEY
-  gpg --homedir /var/ipfire/statusmail/certificates --batch --yes --delete-keys $OLDKEY
+  gpg --homedir /var/ipfire/statusmail/keys --batch --yes --delete-secret-keys $OLDKEY
+  gpg --homedir /var/ipfire/statusmail/keys --batch --yes --delete-keys $OLDKEY
 fi;
